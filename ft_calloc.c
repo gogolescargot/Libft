272bca36114ba7fa7506ecfc38e40100d50e862c
Gauthier Galon <ggalon@z4r4p3.42lyon.fr>
2023-11-17 18:47:28 +0100

    v3

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ggalon <ggalon@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 19:24:25 by ggalon            #+#    #+#             */
-/*   Updated: 2023/11/10 14:54:59 by ggalon           ###   ########.fr       */
+/*   Updated: 2023/11/17 18:46:21 by ggalon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*alloc;
 
-	if (size == 0 || nmemb == 0 || size * nmemb > INT_MAX)
+	if (nmemb > ULONG_MAX / size)
+		return (NULL);
+	if (size == 0 || nmemb == 0)
 		return (malloc(0));
 	alloc = malloc(nmemb * size);
 	if (!alloc)
